55c491fb04fe4d2e047c1ee3d49208b79c988382
doter1995 <1182785541@qq.com>
2017-09-06 17:58:36 +0800

    添加双饼图传送门

--- d3/charts/pies/index.js
+++ d3/charts/pies/index.js
@@ -5,55 +5,136 @@ window.onload = function () {
     var height = window.innerHeight * .9
     var inner = 10
     var outer = 100
-    var svg = d3.select("#root").append('svg')
+    var root = d3.select("#root").append('svg')
         .attr('width', width)
         .attr('height', height)
+    //左饼图区
+    var svg = root.append('g')
+        .attr('class', 'svg1')
+        .attr('transform', 'translate(' + width / 3 + ',' + height / 2 + ')')
+    //右饼图区    
+    var svg1 = root.append('g')
+        .attr('class', 'svg2')
+        .attr('transform', 'translate(' + width / 3 * 2 + ',' + height / 2 + ')')
     var color = d3.scaleOrdinal(d3.schemeCategory20)
-    //准备数据
+    // 准备数据
     var data = [1, 3, 3, 5, 8, 13, 21];
+    var data1 = [1, 3, 3, 5, 8, 13, 21];
     // 构建布局
-    var pies = d3.pie()(data);
-    console.log("pies", pies)
-    //pies存储的是计算后的数据
-    //绘制
+    var pies = d3.pie();
+
     var arc = d3.arc()
         .innerRadius(inner)
         .outerRadius(outer)
+
     //加入拖动
     var draged = d3.drag()
-        .on('drag', function (d) {
+        .on('drag', function (d, i) {
             d.dx += d3.event.dx;
             d.dy += d3.event.dy;
             d3.select(this).attr("transform", "translate(" + d.dx + "," + d.dy + ")");
         })
-    var arcGroup = svg.data(pies)
+        .on('end', function (d, i) {
+            var dis2 = d.dx * d.dx + d.dy * d.dy
+            if (dis2 > outer * outer) {
+                //移动到对方饼图中
+                if (d.type == 0) {
+                    var moveData = data.splice(i, 1);
+                    data1.push(moveData)
+                } else {
+                    var moveData = data1.splice(i, 1);
+                    data.push(moveData)
+                }
+
+
+            } else {
+                d.type=d.type
+                d.dy = 0
+                d.dx = 0
+            }
+            redraw();
+        })
+    console.log(root)
+    //绘制
+    var arcGroup = svg.selectAll('g.arcG').data(pies(data))
         .enter()
         .append('g')
-        .attr('class','arcG')
+        .attr('class', 'arcG')
         .each(function (d) {
-            d.dx = width / 2
-            d.dy = height / 2
+            d.type = 0
+            d.dx = 0
+            d.dy = 0
         })
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .call(draged)
+    var arcGroup1 = svg1.selectAll('g.arcG').data(pies(data1))
+        .enter()
+        .append('g')
+        .attr('class', 'arcG')
+        .each(function (d) {
+            d.type = 1
+            d.dx = 0
+            d.dy = 0
+        })
         .call(draged)
     arcGroup.append('path')
         .attr("fill", function (d, i) { return color(i) })
         .attr("d", arc)
+    arcGroup1.append('path')
+        .attr("fill", function (d, i) { return color(i) })
+        .attr("d", arc)
 
-    setInterval(rerender, 1000)
-    function rerender() {
-        pies = d3.shuffle(pies)
-        arcGroup.data(pies)
-        arcGroup.transition()
-            .duration(500)
+    function redraw() {
+        var update = svg.selectAll('g.arcG')
+            .data(pies(data))
+
+        update.each(function (d) {
+            d.type = 0
+            d.dx = 0
+            d.dy = 0
+        })
+        .attr("transform", function(d){return "translate(" + d.dx + "," + d.dy + ")"})
             .select('path')
+            .attr('d', arc)
+        var enter = update.enter()
+            .append('g')
+            .attr('class', 'arcG')
+            .each(function (d) {
+                d.type = 0
+                d.dx = 0
+                d.dy = 0
+            }) .attr("transform", function(d){return "translate(" + d.dx + "," + d.dy + ")"})
+            .call(draged)
+
+        enter.append('path')
             .attr("fill", function (d, i) { return color(i) })
             .attr('d', arc)
-    }
 
-    function redraw(){
-        //处理 update enter exit
-        var update = svg.selectAll('g.arcG')
-            .data(pies)
+        update.exit().remove();
+
+        var update1 = svg1.selectAll('g.arcG')
+            .data(pies(data1))
+
+        update1.each(function (d) {
+            d.type = 1
+            d.dx = 0
+            d.dy = 0
+        }).attr("transform", function(d){return "translate(" + d.dx + "," + d.dy + ")"})
+            .select('path')
+            .attr('d', arc)
+        var enter1 = update1.enter()
+            .append('g')
+            .attr('class', 'arcG')
+            .each(function (d) {
+                d.type = 1
+                d.dx = 0
+                d.dy = 0
+            }).attr("transform", function(d){return "translate(" + d.dx + "," + d.dy + ")"})
+            .call(draged)
+
+        enter1.append('path')
+            .attr("fill", function (d, i) { return color(i) })
+            .attr('d', arc)
+
+        update1.exit().remove();
     }
 }
